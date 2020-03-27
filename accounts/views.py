f0b9156ec0e7098143af094966687bac1e954ff9
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-03-27 16:15:43 +0000

    added fully functioning login/logout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import auth, messages
 from .forms import UserLoginForm
 
+def login(request):
+    """
+    This returns a login page
+    """
+    """login_form = UserLoginForm()"""
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                    password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+
 def logout(request):
     """
     logs out
@@ -9,10 +29,3 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "You have been logged out")
     return redirect(reverse('home_page'))
-
-def login(request):
-    """
-    This returns a login page
-    """
-    login_form = UserLoginForm()
-    return render(request, 'login.html', {'login_form': login_form})
