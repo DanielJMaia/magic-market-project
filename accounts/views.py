8895f4b0f233623340e1de9c6447b34701ccc782
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-03-28 16:55:54 +0000

    added user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import auth, messages
-from .forms import UserLoginForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserLoginForm, RegistrationForm
+from cards.models import Card
 
 def login(request):
     """
     This returns a login page
     """
     """login_form = UserLoginForm()"""
+    if request.user.is_authenticated:
+        return redirect(reverse('home_page'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
         if login_form.is_valid():
@@ -15,17 +19,45 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('home_page'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
         login_form = UserLoginForm()
     return render(request, 'login.html', {'login_form': login_form})
 
-
+@login_required
 def logout(request):
     """
     logs out
     """
+    cards = Card.objects.order_by('-listing_views')
     auth.logout(request)
     messages.success(request, "You have been logged out")
     return redirect(reverse('home_page'))
+
+def register(request):
+    """
+    registers a user
+    """
+    if request.user.is_authenticated:
+        return redirect(reverse('home_page'))
+    if request.method == "POST":
+        registration_form = RegistrationForm(request.POST)
+        if registration_form.is_valid():
+            user = registration_form.save()
+
+            user = auth.authenticate(username=request.POST['username'],
+            password=request.POST['password1'])
+
+            if user:
+                auth.login(user=user, request=request),
+                messages.success(request, "You have been successfully registered")
+                return redirect('home_page')
+            else:
+                messages.error(request, "Unable to register your account, please try again later")
+    else:            
+        registration_form = RegistrationForm()
+        
+    return render(request, 'register.html', {
+        "registration_form": registration_form})
