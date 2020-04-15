55136e3b8f208b7452bb7b8ebd0c190958c726f2
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-04-15 14:30:52 +0000

    got hoverimage working in cards.html

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,18 +1,3 @@
-//var i;
-//for (i = 0; i < "//I need this to be the max amount of items in my list"; i++) {
-//
-//  document.getElementById("hide-image-" + i).addEventListener("click", imageToggleHide);
-//    document.getElementById("show-image-" + i).addEventListener("click", imageToggleShow);
-//
-//    function imageToggleHide() {
-//        document.getElementById("card-image-test-" + i).style.display = "none";
-//    };
-
-//    function imageToggleShow() {
-//        document.getElementById("card-image-test-" + i).style.display = "block";
-//    };
-//}
-
 function imageToggleHide(the_id) {
         document.getElementById(the_id).style.display = "none";
     };
