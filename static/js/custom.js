4b5181bb7315f1ed07a9b0f0d466ad05d3b994db
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-04-15 13:20:15 +0000

    fixed show/hide buttons on home page

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,14 +1,22 @@
-var i;
-for (i = 0; i < "//I need this to be the max amount of items in my list"; i++) {
+//var i;
+//for (i = 0; i < "//I need this to be the max amount of items in my list"; i++) {
+//
+//  document.getElementById("hide-image-" + i).addEventListener("click", imageToggleHide);
+//    document.getElementById("show-image-" + i).addEventListener("click", imageToggleShow);
+//
+//    function imageToggleHide() {
+//        document.getElementById("card-image-test-" + i).style.display = "none";
+//    };
 
-    document.getElementById("hide-image-" + i).addEventListener("click", imageToggleHide);
-    document.getElementById("show-image-" + i).addEventListener("click", imageToggleShow);
+//    function imageToggleShow() {
+//        document.getElementById("card-image-test-" + i).style.display = "block";
+//    };
+//}
 
-    function imageToggleHide() {
-        document.getElementById("card-image-test-" + i).style.display = "none";
+function imageToggleHide(the_id) {
+        document.getElementById(the_id).style.display = "none";
     };
 
-    function imageToggleShow() {
-        document.getElementById("card-image-test-" + i).style.display = "block";
+function imageToggleShow(the_id) {
+        document.getElementById(the_id).style.display = "block";
     };
-}
