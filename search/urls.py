030eb2c84c53be476b41327e85aa49430cf09196
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-04-26 16:45:33 +0000

    added advanced search page and views in search app

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import do_search
+from .views import basic_search, search_page, advanced_search
 
 
 urlpatterns = [
-    url(r'^$', do_search, name='search')
+    url(r'^$', basic_search, name='search'),
+    url(r'^search', search_page, name='search_page'),
+    url(r'^advanced$', advanced_search, name='advanced_search'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from cards.models import Card
 
+def basic_search(request):
+    cards = Card.objects.filter(card_title__icontains=request.GET['search_title'])
+    return render(request, 'cards.html', {'cards': cards})
 
-def do_search(request):
-    cards = Card.objects.filter(card_title__icontains=request.GET['q'])
+
+def search_page(request):
+    return render(request, 'search.html')
+
+
+def advanced_search(request):
+    cards = Card.objects.filter(
+        card_title__icontains=request.GET['title'],
+        card_edition__icontains=request.GET['edition'],
+        card_condition__icontains=request.GET['condition'])
     return render(request, 'cards.html', {'cards': cards})
