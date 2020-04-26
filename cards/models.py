655399250c1da96be48a48d073c1fe03f5e18b61
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-04-26 17:14:46 +0000

    changed card condition to dropdown menu

--- cards/models.py
+++ cards/models.py
@@ -6,10 +6,31 @@ class Card(models.Model):
     """
     This is going to be a single card being sold
     """
+    MINT = 'M'
+    NEAR_MINT = 'NM'
+    EXCELLENT = 'EX'
+    GOOD = 'GD'
+    LIGHTLY_PLAYED = 'LP'
+    PLAYED = 'PL'
+    POOR = 'P'
+    CARD_CONDITION_CHOICES = [
+        (MINT, 'Mint'),
+        (NEAR_MINT, 'Near Mint'),
+        (EXCELLENT, 'Excellent'),
+        (GOOD, 'Good'),
+        (LIGHTLY_PLAYED, 'Lightly Played'),
+        (PLAYED, 'Played'),
+        (POOR, 'Poor'),
+    ]
+
     card_title = models.CharField(max_length=200)
     card_description = models.TextField()
     card_edition = models.TextField()
-    card_condition = models.TextField()
+    card_condition = models.CharField(
+        max_length=2,
+        choices=CARD_CONDITION_CHOICES,
+        default=NEAR_MINT
+    )
     card_price = models.DecimalField(max_digits=7, decimal_places=2, default='0')
     listing_created_date = models.DateTimeField(auto_now_add=True)
     listing_published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
--- search/views.py
+++ search/views.py
@@ -14,5 +14,5 @@ def advanced_search(request):
     cards = Card.objects.filter(
         card_title__icontains=request.GET['title'],
         card_edition__icontains=request.GET['edition'],
-        card_condition__icontains=request.GET['condition'])
+        card_condition=request.GET['condition'])
     return render(request, 'cards.html', {'cards': cards})
