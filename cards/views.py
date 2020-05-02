f62b50e4e614d63e775c9f4db14b17b7ed44e387
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-05-02 15:10:55 +0000

    re-added accidentally removed view_specific_card url

--- cards/views.py
+++ cards/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from django.contrib import auth, messages
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Card
 from .forms import CardDescForm
 
@@ -16,7 +15,7 @@ def get_cards(request):
     Create a view that will return a list of cards that have been
     uploaded to the website by users
     """
-    card_list = Card.objects.all()
+    cards = Card.objects.all()
     return render(request, "cards.html", {'cards': cards})
 
 
