debaa247d0fd84e6a19bd575769eb55bd4b4c20b
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-05-04 16:04:37 +0000

    tried changing media root with mediafiles_dirs

--- magicmarket/settings.py
+++ magicmarket/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 #import env
+import dj_database_url
 
 # settings.py
 from dotenv import load_dotenv
@@ -20,8 +21,6 @@ from dotenv import load_dotenv
 from pathlib import Path  # python3 only
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
-import dj_database_url
 
 
 
@@ -169,7 +168,12 @@ MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS = (
+    os.path.join(BASE_DIR, 'media')
+)
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
