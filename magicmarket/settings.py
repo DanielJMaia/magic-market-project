e3e92a2a4e409fceb0b9d24f0a35e2d57749e0e5
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-05-04 15:28:28 +0000

    removed * from allowed hosts

--- magicmarket/settings.py
+++ magicmarket/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('C9_HOSTNAME'), '8000-a4163a0e-8e4e-49e4-8aca-e7a98fe1051c.ws-eu01.gitpod.io', 'magic-market.herokuapp.com', 'magic-market.herokuapp.com/', '*']
+ALLOWED_HOSTS = [os.getenv('C9_HOSTNAME'), '8000-a4163a0e-8e4e-49e4-8aca-e7a98fe1051c.ws-eu01.gitpod.io', 'magic-market.herokuapp.com', 'magic-market.herokuapp.com/', '8080-a4163a0e-8e4e-49e4-8aca-e7a98fe1051c.ws-eu01.gitpod.io']
 
 
 # Application definition
