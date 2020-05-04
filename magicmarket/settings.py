532c26dc8a66a219c7bc368751f5de280c5dd128
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-05-04 16:37:01 +0000

    added MEDIA URL instead of Media root to urls.py

--- magicmarket/settings.py
+++ magicmarket/settings.py
@@ -164,8 +164,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
 MEDIAFILES_LOCATION = 'media'
 DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-MEDIA_URL = '"https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
 STRIPE_SECRET = os.getenv('STRIPE_SECRET')
