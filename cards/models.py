209336f3d6fab76192e1ad31d2da237c1f05a35a
Daniel Johnson Maia <djohnsonmaia@gmail.com>
2020-05-06 12:25:08 +0000

    added quantity as upper limit to amount someone can order

--- cards/models.py
+++ cards/models.py
@@ -36,6 +36,7 @@ class Card(models.Model):
     listing_created_date = models.DateTimeField(auto_now_add=True)
     listing_published_date = models.DateTimeField(blank=True, null=True, default=timezone.now)
     listing_views = models.IntegerField(default=0)
+    card_amount = models.IntegerField(default=1)
     image = models.ImageField(upload_to="img", blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, default='0')
 
